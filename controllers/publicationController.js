c20ec220dfa3df431a6efe32f4877c7aa38bbea9
Borislav Vezirov <bobi.vez@gmail.com>
2022-02-25 11:33:22 +0200

    added edit page and functionality

--- controllers/publicationController.js
+++ controllers/publicationController.js
@@ -1,5 +1,6 @@
 const { isUser } = require('../middlewares/guards.js');
 const { createPost } = require('../services/publicationServices.js');
+const { createPost, getOneById, getOneAndUpdate, deletePost, share } = require('../services/publicationServices.js');
 const mapErrors = require('../utils/mappers.js');
 
 const router = require('express').Router();
@@ -26,6 +27,7 @@ router.post('/create', isUser(), async (req, res) => {
     try {
         
         await createPost(post);
+        await createPost(post, req.session.user.username);
 
         res.redirect('/gallery');
 
@@ -37,5 +39,41 @@ router.post('/create', isUser(), async (req, res) => {
     }
 });
 
+router.get('/edit/:id', isUser(), async (req, res) => {
+    
+    const id = req.params.id;
+
+    const post = await getOneById(id);
+
+    if(req.session.user._id != post.author._id){
+
+        return res.redirect('/login');
+    };
+ 
+
+    res.render('edit', { title: 'Edit Page', post })
+});
+
+router.post('/edit/:id', isUser(), async (req, res) => {
+    
+    const id = req.params.id;
+
+    const existing = await getOneById(id);
+
+    if(req.session.user._id != existing.author._id){
+
+        return res.redirect('/login');
+    };
+
+    try {
+        const post = await getOneAndUpdate(id, req.body);
+        res.redirect('/details/' + id);
+        
+    } catch (error) {
+        const errors = mapErrors(error);
+        res.render('edit', { title: 'Edit Page', errors});
+        
+    }
+});
 
 module.exports = router; 
--- services/publicationServices.js
+++ services/publicationServices.js
@@ -1,17 +1,40 @@
 const Publication = require("../models/Publication.js");
+const { getUserByUsername } = require("./userServices.js");
 
 async function createPost(post){
 
     const result = new Publication(post);
+async function createPost(post, username){
 
     await result.save();
+    const createdPost = new Publication(post);
 
     return result;
+    const user = await getUserByUsername(username);
+
+    user.publications.push(createdPost._id);
+
+    await user.save();
+
+    await createdPost.save();
+
+    return createdPost;
 };
 
 async function getAllPost(){
 
     return await Publication.find({}).lean();
+};
+
+async function getOneById(id){
+
+    return await Publication.findById(id).populate('author', 'username').lean();
+};
+
+async function getOneAndUpdate(id, post){
+
+    return await Publication.findByIdAndUpdate(id, post);
+
 };
 
 
@@ -21,4 +44,7 @@ async function getAllPost(){
 module.exports = {
     createPost,
     getAllPost
+    getAllPost,
+    getOneById,
+    getOneAndUpdate,
 };
